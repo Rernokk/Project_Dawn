a7342eef9d965dcae05be298e71279f0d67f8e30
Rernokk <ssmith37@uccs.edu>
2017-08-29 01:26:14 -0600

    Initial Controls

---
+++ Project_Dawn/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+    [SerializeField]
+    KeyCode left = KeyCode.A, right = KeyCode.D, jump = KeyCode.Space;
+
+    [SerializeField]
+    float playerSpeed = 1f, verticalJump = 1f;
+
+    [SerializeField]
+    bool grounded = true;
+    Rigidbody2D rgd;
+    void Start () {
+        rgd = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(left))
+        {
+            rgd.AddForce(-transform.right * Time.deltaTime * playerSpeed, ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(right))
+        {
+            rgd.AddForce(transform.right * Time.deltaTime * playerSpeed, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rgd.velocity += new Vector2(0,1) * verticalJump * Time.deltaTime;
+            grounded = false;
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
