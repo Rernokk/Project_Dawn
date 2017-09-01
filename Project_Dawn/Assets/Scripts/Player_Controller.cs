3992366fea2bb45d3133ea2bf9309db248a716aa
Rernokk <ssmith37@uccs.edu>
2017-09-01 02:17:28 -0600

    Established Basic Movement

--- Project_Dawn/Assets/Scripts/Player_Controller.cs
+++ Project_Dawn/Assets/Scripts/Player_Controller.cs
@@ -9,9 +9,12 @@ public class Player_Controller : MonoBehaviour {
     [SerializeField]
     float playerSpeed = 1f, verticalJump = 1f;
     float gravScale = 1f;
+
     [SerializeField]
     bool grounded = true;
+    
     Rigidbody2D rgd;
+
     void Start () {
         rgd = GetComponent<Rigidbody2D>();
         gravScale = rgd.gravityScale;
@@ -28,15 +31,33 @@ public class Player_Controller : MonoBehaviour {
             rgd.AddForce(transform.right * Time.deltaTime * playerSpeed, ForceMode2D.Impulse);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (Input.GetKey(KeyCode.Space) && grounded)
         {
             rgd.velocity += new Vector2(0,1) * verticalJump;
             grounded = false;
+        }
+
+        if (rgd.velocity.y < 0 && rgd.gravityScale == gravScale)
+        {
+            rgd.gravityScale *= 1.5f;
+        }
+
+        if (rgd.velocity.y >= 0 && rgd.gravityScale != gravScale)
+        {
+            rgd.gravityScale = gravScale;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            grounded = true;
+        }
     }
 }
