87c06fbb48d0ee515958e02936c37de633657fe7
Rernokk <ssmith37@uccs.edu>
2017-09-16 00:49:32 -0600

    Introducing Ifrit

---
+++ Project_Dawn/Assets/Scripts/Enemies/Bosses/Ifrit/Boss_Init.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Init : MonoBehaviour {
+    Transform Cage, TriggerZone, BossCamera, Player;
+
+    void Start()
+    {
+        Cage = transform.Find("BossCage");
+        TriggerZone = transform.Find("Start_Trigger");
+        BossCamera = transform.Find("BossCamera");
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //Setting up Arena
+        BossCamera.gameObject.SetActive(false);
+        Cage.gameObject.SetActive(false);
+    }
+	
+	void Update () {
+		
+	}
+
+    public void TriggerFight()
+    {
+        BossCamera.gameObject.SetActive(true);
+        Cage.gameObject.SetActive(true);
+        TriggerZone.gameObject.SetActive(false);
+        Player.transform.Find("Main Camera").gameObject.SetActive(false);
+    }
+}
---
+++ Project_Dawn/Assets/Scripts/Enemies/Bosses/Ifrit/CurseOfFlameAura.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurseOfFlameAura : MonoBehaviour {
+  [SerializeField]
+  float duration = 1f, range = 1f;
+  Transform player;
+	// Use this for initialization
+	void Start () {
+    player = GameObject.FindGameObjectWithTag("Player").transform;
+    StartCoroutine(DebuffCountdown());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  public void ApplyDebuff()
+  {
+
+  }
+
+  IEnumerator DebuffCountdown()
+  {
+    yield return new WaitForSeconds(duration);
+    if (Vector2.Distance(player.transform.position, transform.position) <= range)
+    {
+      transform.parent.GetComponent<Ifrit_FSM>().AuraMult = 1;
+    }
+  }
+}
