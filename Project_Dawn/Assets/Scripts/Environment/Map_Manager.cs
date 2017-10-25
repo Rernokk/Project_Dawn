b2da8deade5f47b3921c35b4efdb318fb031a89d
Rernokk <ssmith37@uccs.edu>
2017-10-25 14:27:00 -0600

    Map Manager tweaks and bitmasking finalization. I am now able to generate maps in 2D by pixel-layout.

--- Project_Dawn/Assets/Scripts/Environment/Map_Manager.cs
+++ Project_Dawn/Assets/Scripts/Environment/Map_Manager.cs
@@ -20,9 +20,9 @@ public class Map_Manager : MonoBehaviour
   #endregion
 
   #region Variables
-  private static Map_Manager instance;
-  Tile[,] levelArray;
-  int mapWidth, mapHeight;
+  private static Map_Manager instance = null;
+  static Tile[,] levelArray;
+  static int mapWidth, mapHeight;
   #endregion
 
   #region Properties
@@ -50,8 +50,13 @@ public class Map_Manager : MonoBehaviour
   #region Private Methods
   private Map_Manager() { }
 
-  private void Start()
+  private void Awake()
   {
+    if(instance != null && instance != this){
+      Destroy(this.gameObject);
+    }
+
+    instance = this;
     DontDestroyOnLoad(gameObject);
   }
 
@@ -87,7 +92,6 @@ public class Map_Manager : MonoBehaviour
     levelArray = new Tile[myPixelMap.width, myPixelMap.height];
     mapWidth = myPixelMap.width;
     mapHeight = myPixelMap.height;
-    print("Map Width: " + mapWidth + " & Map Height: " + mapHeight);
     for (int i = 0; i < myPixelMap.width; i++)
     {
       for (int j = 0; j < myPixelMap.height; j++)
@@ -109,33 +113,32 @@ public class Map_Manager : MonoBehaviour
     }
 
     ////Spawning Tiles into worldspace.
-    //Transform LevelHolder = GameObject.Find("Level_Loader").transform;
-    //for (int i = 0; i < myPixelMap.width; i++)
-    //{
-    //  for (int j = 0; j < myPixelMap.height; j++)
-    //  {
-    //    if (levelArray[i, j].isOccupied)
-    //    {
-    //      Instantiate(GetTileObject(pairListing, levelArray[i, j].myIDColor), position: new Vector3(i, j, 0), rotation: Quaternion.identity).transform.parent = LevelHolder;
-    //    }
-    //  }
-    //}
-    StartCoroutine(StallCreation(myPixelMap, pairListing));
+    Transform LevelHolder = GameObject.Find("Level_Loader").transform;
+    for (int i = 0; i < myPixelMap.width; i++)
+    {
+      for (int j = 0; j < myPixelMap.height; j++)
+      {
+        if (levelArray[i, j].isOccupied)
+        {
+          Instantiate(GetTileObject(pairListing, levelArray[i, j].myIDColor), position: new Vector3(i, j, 0), rotation: Quaternion.identity).transform.parent = LevelHolder;
+        }
+      }
+    }
   }
 
 
   public int IsTileOpen(Vector2 pos){
-    if ((pos.x < 0 || pos.x >= mapWidth) && (pos.y < 0 || pos.y >= mapHeight))
+    
+    if ((pos.x < 0 || pos.x >= mapWidth) || (pos.y < 0 || pos.y >= mapHeight))
     {
-      print("Out of Range, W: " + mapWidth + ", H: " + mapHeight);
+      //Debugger, throw -1 for out of range.
       return 0;
     }
     if (levelArray[(int) pos.x, (int) pos.y].isOccupied){
-      print("Occupied");
+      //Occupied, use in calculation
       return 1;
     }
-
-    print("Not Occupied");
+    //Null Calculation Value.
     return 0;
   }
   #endregion
---
+++ Project_Dawn/Assets/Scripts/Util/CSVReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CSVReader : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.K)){
+      Bitmasking_Value tracker = GetComponent<Bitmasking_Value>();
+      tracker.IndexLookup = new List<Bitmasking_Value.BitmaskPairValue>();
+      StreamReader reader = new StreamReader("Assets/References/PairsReordered.csv");
+      for (int i = 0; i < 255; i++){
+        string line = reader.ReadLine();
+        int myVal = int.Parse(line.Substring(0, line.IndexOf(",")));
+        int myIndex = int.Parse(line.Substring(line.IndexOf(",")+1));
+        tracker.IndexLookup.Add(new Bitmasking_Value.BitmaskPairValue(myIndex, myVal));
+      }
+    }
+	}
+}
